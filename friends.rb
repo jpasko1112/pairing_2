ffebcaae2f32f1f82b962a52fde663df53d6f9ea
Justin Pasko <jpasko1112@gmail.com>
2023-02-08 20:26:30 -0500

    Add arrays elements & vacations destinations

--- friends.rb
+++ friends.rb
@@ -2,3 +2,13 @@ arrays = ['walt', 'disney', 'mickey', 'daffy', 'pluto']
 puts arrays[2]
 puts arrays[4]
 puts arrays[0]
+
+vacations = ['france', 'Guam', 'Mexico', 'Canada', 'Africa']
+
+vacations.each do |vacation|
+    if vacation == 'france'
+        puts 'ready to go!'
+    else
+        puts "well i guess ill got to #{vacation}"
+    end 
+end
